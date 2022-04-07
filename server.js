d16538b6469206416f54aaa762d38c9dbefb8a37
Jessica Wang <jessikka@email.unc.edu>
2022-04-07 17:02:44 -0400

    added id column to db table

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (help) {
     const db = new Database('log.db')
 
     // create table
-    const sqlInit = `CREATE TABLE IF NOT EXISTS accesslog (
+    const sqlInit = `CREATE TABLE IF NOT EXISTS accesslog (id INTEGER PRIMARY KEY,
             remoteaddr TEXT, remoteuser TEXT, time TEXT, method TEXT, url TEXT, protocol TEXT,
             httpversion TEXT, secure TEXT, status INTEGER, referer TEXT, useragent TEXT
         )`;
